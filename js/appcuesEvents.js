b75b1377475b1fcf578960942c70b4429e561048
Kien <kdang@coveo.com>
2021-11-08 16:03:14 -0500

    chore(userflow): hardcoding user for demo purposes

--- js/appcuesEvents.js
+++ js/appcuesEvents.js
@@ -5,7 +5,15 @@ function appcuesInit(orgId) {
 document.addEventListener("DOMContentLoaded", function() {
     const orgId = localStorage.getItem('organizationId')
     const isFromCoveoDev = localStorage.getItem('coveoDev')
+
     if(orgId) {
         appcuesInit(orgId, isFromCoveoDev)
     }
+
+
+    userflow.identify('test123', {
+    name: 'Kien Dang',
+    email: 'kdang@coveo.com',
+    signed_up_at: '2021-10-28T21:05:35.000+00:00',
+    });
   });
